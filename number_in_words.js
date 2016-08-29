c4303da4d46939dc12efdd98705b5244604d73b8
Aji Lantang Mardika & Andrew Tandiawan <pair+ajilantang.atandiawan@hacktiv8.com>
2016-08-29 19:12:05 +0700

    Number in words

--- number_in_words.js
+++ number_in_words.js
@@ -1,6 +1,53 @@
-function in_words(angka){
-  // Your code here
-}
+let array = [];
+let convertAja = function (digitAngka){
+  switch (digitAngka) {
+    case 1: return 'satu'
+    break;
+    case 2: return 'dua'
+    break;
+    case 3: return 'tiga'
+    break;
+    case 4: return 'empat'
+    break;
+    case 5: return 'lima'
+    break;
+    case 6: return 'enam'
+    break;
+    case 7: return 'tujuh'
+    break;
+    case 8: return 'delapan'
+    break;
+    case 9: return 'sembilan'
+    break;
+    case 0: return ''
+    break;
+    default: console.log("eror");
 
+  }
+}
+let satuanAngka = ["","puluh","ratus","ribu","puluhribu","ratusribu","juta"]
+let words = ""
+function in_words(angka){
+    let reminder = angka;
+    for (i = 6; i >=0; i--){
+      array[i] = Math.floor(reminder/Math.pow(10,i));
+      reminder = reminder%Math.pow(10,i);
+      if (array[i]===0){
+        null
+      }else{
+        words = words +convertAja(array[i])+ " "+ satuanAngka[i] + " ";
+      }
+
+    }
+    return words
+//e here
+}
+//jutaan
+//ratusan tibu
+//puluhan ribu
+//ribuan
+//ratusan
+//puluhan
+//satuan
 // Driver code
-console.log(in_words(1000000));
+console.log(in_words(1356));
