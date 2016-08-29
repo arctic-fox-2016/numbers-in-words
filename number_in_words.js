63b0e31cb5f5109e720c153870c4f7a4116cecd3
Elkana Lumbantobing & Fadhli Ramadhani Arislan <pair+elkana-ltobing.fadhlirams@hacktiv8.com>
2016-08-29 20:39:06 +0700

    numbers in words

--- number_in_words.js
+++ number_in_words.js
@@ -1,6 +1,27 @@
+
+
 function in_words(angka){
-  // Your code here
-}
+var jutaan = ['','ribu','juta'];
+var satuan = ['','satu','dua','tiga','empat', 'lima','enam','tujuh','delapan','sembilan'];
+var belasan = ['sepuluh','sebelas','duabelas','tigabelas', 'empatbelas','limabelas','enambelas', 'tujuhbelas','delapanbelas','sembilanbelas'];
+var puluhan = ['duapuluh','tigapuluh','empatpuluh','limapuluh', 'enampuluh','tujuhpuluh','delapanpuluh','sembilanpuluh'];
+
+function test(angka) {
+
+  var depanAngka ="";
+  var bilanganHuruf ="";
+
+  for(var i=0;i<puluhanangka.length;i++){
+    while(value >= puluhanangka[i]) {
+      value = Math.floor(angka/puluhanangka[i]);
+      bilanganHuruf += puluhanhuruf[i];
+      console.log(value);
+       
+      }
+    }
+
+    return totalangka = belashuruf[i+1] + bilanganHuruf;
+  }
 
 // Driver code
 console.log(in_words(1000000));
