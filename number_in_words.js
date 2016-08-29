e7ebd31dd1b6b3dee65026847cb813092b0e34a0
Ari Adiprana & Fansyah Faturahman <pair+ariadiprana.fansyah@hacktiv8.com>
2016-08-29 19:57:53 +0700

    fix puluh ribu

--- number_in_words.js
+++ number_in_words.js
@@ -4,13 +4,36 @@ function in_words(angka){
   let puluh = 0;
   let ratusan = 0;
   let ribuan = 0;
+  let puluhribu = 0;
+  let ratusribu = 0;
   let hasil = "";
   let depan = "";
 
   sisa = angka%10;
   puluh = Math.floor(angka/10);
-  ratusan = Math.floor(angka/100);3179
+  ratusan = Math.floor(angka/100);
   ribuan = Math.floor(angka/1000);
+  puluhribu= Math.floor(angka/10000);
+  ratusribu= Math.floor(angka/100000);
+
+  if (ratusribu>0){
+    hasil+=cetak(ratusribu);
+    hasil+=' ratus';
+    puluhribu = puluhribu%10000;
+    puluhribu = puluhribu%1000;
+    puluhribu = puluhribu%100;
+    puluhribu = puluhribu%10;
+  }
+
+
+  if (puluhribu>0){
+    hasil+=cetak(puluhribu);
+    hasil+=' puluh';
+    ribuan = ribuan%1000;
+    ribuan = ribuan%100;
+    ribuan = ribuan%10;
+
+  }
 
   if(ribuan>0)
   {
@@ -31,18 +54,28 @@ function in_words(angka){
   if(puluh==1){
     hasil+=cetak(sisa)
     hasil+=' belas';
+//console.log("Sebelumkonversi : "+hasil);
+    hasil = konversi(hasil);
     return hasil;
   }
   if(puluh>0)hasil+=cetak(puluh);
   if(puluh>1&&puluh<10)hasil +=' puluh';
   hasil+=cetak(sisa);
-
-  hasil = hasil.replace("satu ratus", "seratus");
-  hasil = hasil.replace("satu ribu", "seribu");
-
+  hasil = konversi(hasil);
 
 return hasil;
 
+}
+
+function konversi(hasil){
+  for(var i = 0 ; i<5 ; i++){
+      hasil = hasil.replace("satu belas", "sebelas");
+      hasil = hasil.replace("satu ratus", "seratus");
+      hasil = hasil.replace("satu ribu", "seribu");
+      hasil = hasil.replace("satu puluh seribu", "sebelas ribu");
+  }
+
+  return hasil;
 }
 
 function cetak(sisa)
@@ -61,8 +94,11 @@ function cetak(sisa)
 }
 
 // Driver code
+console.log(in_words(328234));
+console.log(in_words(11111));
 console.log(in_words(9999));
-console.log(in_words(121));
+console.log(in_words(111));
+console.log(in_words(101));
 console.log(in_words(201));
 console.log(in_words(7));
 console.log(in_words(99));
